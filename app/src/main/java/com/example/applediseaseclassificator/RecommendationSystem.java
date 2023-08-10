a2d7abca4a18ba79de867ecc6fc92ef8ed02f654
DESKTOP-9KFOJ7S\Josip <rizner.josip@gmail.com>
2023-08-10 15:31:18 +0200

    Machine learning refactor

--- app/src/main/java/com/example/applediseaseclassificator/RecommendationSystem.java
+++ app/src/main/java/com/example/applediseaseclassificator/RecommendationSystem.java
@@ -3,20 +3,33 @@ package com.example.applediseaseclassificator;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.List;
+
 public class RecommendationSystem {
 
-    private String id, latitude, longitude, name, startingImageReference;
+    private String id, latitude, longitude, name, startingImageReference, classifiedDisease;
     private long timestamp;
+    private List<Float> startDiseaseConfidences;
 
     public RecommendationSystem (){}
 
-    public RecommendationSystem (String id, String latitude, String longitude, String name, String imageReference){
+    public RecommendationSystem (String id, String latitude, String longitude, String name, String imageReference, String classifiedDisease, List<Float> startDiseaseConfidences){
         this.id = id;
         this.latitude = latitude;
         this.longitude = longitude;
         this.name = name;
         this.startingImageReference = imageReference;
         this.timestamp = System.currentTimeMillis();
+        this.classifiedDisease = classifiedDisease;
+        this.startDiseaseConfidences = startDiseaseConfidences;
+    }
+
+    public String getClassifiedDisease() {
+        return classifiedDisease;
+    }
+
+    public List<Float> getStartDiseaseConfidences() {
+        return startDiseaseConfidences;
     }
 
     public String getId() {
